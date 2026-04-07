ddeae0a95ad2fc474a3cd849e2d3dfee815068bc
ahmadfaeyzaarsyi-pixel <ahmadfaeyzaarsyi@gmail.com>
2026-04-07 11:41:12 +1000

    Add files via upload

---
+++ arlechino.js
@@ -0,0 +1,17 @@
+
+function munculkanpesan() {
+   var buka =document.getElementById("opened")
+buka.classList.remove("hiden")
+}
+function munculkanpesan1() {
+    var buka =document.getElementById("photo1")
+    buka.classList.remove("hiden")
+}
+function munculkanpesan2() {
+    var buka=document.getElementById("photo2")
+    buka.classList.remove("hiden")
+}
+function mulaimusic() {
+var music =document.getElementById("hbd")
+music.play()
+}
